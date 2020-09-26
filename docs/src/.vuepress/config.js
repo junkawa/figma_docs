a32c496ab7a0f3da20e812bca3691519670460a3
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2020-09-26 22:58:18 +0900

    Adapt lint rules to .js

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,10 +1,10 @@
-const { description } = require("../../package");
+const {description} = require('../../package');
 
 module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: "デザインツール Figma の紹介",
+  title: 'デザインツール Figma の紹介',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -16,11 +16,11 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ["meta", { name: "theme-color", content: "#3eaf7c" }],
-    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+    ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
     [
-      "meta",
-      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+      'meta',
+      {name: 'apple-mobile-web-app-status-bar-style', content: 'black'},
     ],
   ],
 
@@ -30,23 +30,28 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: "",
+    repo: '',
     editLinks: false,
-    docsDir: "",
-    editLinkText: "",
-    lastUpdated: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: true,
     nav: [
       {
-        text: "日本語化拡張",
+        text: '日本語化拡張',
         link:
-          "https://chrome.google.com/webstore/detail/figma-%E6%97%A5%E6%9C%AC%E8%AA%9E%E5%8C%96/ekodckpnalonppfcddgnkoapajahbahc?hl=ja",
+          'https://chrome.google.com/webstore/detail/figma-%E6%97%A5%E6%9C%AC%E8%AA%9E%E5%8C%96/ekodckpnalonppfcddgnkoapajahbahc?hl=ja',
       },
     ],
-    sidebar: {},
+    sidebar: [
+      ['/', '画面構成'],
+      ['/toolbar/', 'ツールバー'],
+      ['/layersPanel/', 'レイヤーパネル'],
+      ['/propertiesPanel/', 'プロパティパネル'],
+    ],
   },
 
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
 };

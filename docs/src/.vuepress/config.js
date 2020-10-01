05efa2eee232e3b66163a5fe75128f9d9ebf9d1f
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2020-10-01 16:34:25 +0900

    Add sitemap

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -53,5 +53,12 @@ module.exports = {
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom',
+    [
+      'sitemap',
+      {
+        hostname: 'https://figma.muise.jp',
+      },
+    ],
+  ],
 };
